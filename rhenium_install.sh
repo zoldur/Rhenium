16db7e079a0c3e545e58b4dcbca9c05e26a4114c
Zoldur <36329578+zoldur@users.noreply.github.com>
2018-06-15 20:40:23 +0100

    Update rhenium_install.sh

--- rhenium_install.sh
+++ rhenium_install.sh
@@ -124,6 +124,26 @@ maxconnections=256
 masternode=1
 externalip=$NODEIP:$COIN_PORT
 masternodeprivkey=$COINKEY
+addnode=140.82.56.130
+addnode=149.28.104.244
+addnode=159.203.43.107
+addnode=163.44.120.190
+addnode=185.204.216.98
+addnode=185.239.237.170
+addnode=188.201.198.118
+addnode=202.182.97.129
+addnode=207.148.16.238
+addnode=207.246.74.90
+addnode=209.222.30.7
+addnode=217.69.0.34
+addnode=45.32.193.85
+addnode=45.63.11.28
+addnode=45.76.7.253
+addnode=45.77.47.79
+addnode=51.15.246.238
+addnode=8.9.11.159
+addnode=80.240.24.178
+addnode=108.61.75.66
 EOF
 }
 
